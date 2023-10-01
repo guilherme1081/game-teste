adf438d5460bab4392c8e877f19f4d26b80fa6bd
Jose Edmario <56650001+JEdmario16@users.noreply.github.com>
2023-10-01 11:47:17 -0300

    Refatorado dash

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var _state_machine
+var _dash_timer
 
 @export_category("Variables")
 @export var _moviment_speed: float = 64.0
@@ -11,26 +12,44 @@ var _state_machine
 @export var _animation_tree: AnimationTree = null
 
 @export_category("Stats")
-@export var dash_amount: int = 3
+@export var dash_cdr: float = 3
+
+var _direction = null
+
+const _dash_duration: float = 0.3
+var _current_dash_duration: float = 0
+var _has_dash: bool = true
 
 func _ready():
 	_state_machine = _animation_tree["parameters/playback"]
-
+	_dash_timer = $dash_timer
+	
 func _physics_process(delta):
 	_move()
 	_animate()
+	_dash()
 	move_and_slide()
-
-func _move() -> void:
-	var _direction: Vector2 = Vector2(
-		Input.get_axis("left", "right"),
-		Input.get_axis("up", "down")
-	)
+	
 	
 
+func _process(delta):
+	if _current_dash_duration > 0:
+		_current_dash_duration -= delta
+		
+		position.x += _direction.x * 3
+		position.y += _direction.y * 3
+				
+func _move() -> void:
+	_direction = Vector2(
+	Input.get_axis("left", "right"),
+	Input.get_axis("up", "down")
+)
+	
 	if _direction != Vector2.ZERO:
 		_animation_tree["parameters/idle/blend_position"] = _direction
 		_animation_tree["parameters/run/blend_position"] = _direction
+		_animation_tree["parameters/dash/blend_position"] = _direction
+		
 		
 		velocity.x = lerp(velocity.x, _direction.normalized().x*_moviment_speed, _friction)
 		velocity.y = lerp(velocity.y, _direction.normalized().y*_moviment_speed, _friction)
@@ -40,7 +59,24 @@ func _move() -> void:
 		
 
 func _animate() -> void:
+	
+	if _current_dash_duration > 0:
+		_state_machine.travel("dash")
+		return
 	if velocity.length() > 8:
 		_state_machine.travel("run")
 		return
 	_state_machine.travel("idle")
+
+func _dash() -> void:
+	if Input.is_action_pressed("dash") and _has_dash:
+		_has_dash = false
+		_current_dash_duration = _dash_duration
+		_dash_timer.start(dash_cdr)
+	
+	
+
+
+func _on_dash_timer_timeout():
+	_has_dash = true
+	_moviment_speed = 64
